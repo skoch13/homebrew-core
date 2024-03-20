8404b0a4af25893cc9fb25d9ae2177f089aa7fbb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-03-20 13:55:31 +0800

    qt: bump revision (llvm 18.1.1)

--- Formula/q/qt.rb
+++ Formula/q/qt.rb
@@ -10,7 +10,7 @@ class Qt < Formula
     { "GPL-3.0-only" => { with: "Qt-GPL-exception-1.0" } },
     "LGPL-3.0-only",
   ]
-  revision 1
+  revision 2
   head "https://code.qt.io/qt/qt5.git", branch: "dev"
 
   stable do
@@ -79,7 +79,6 @@ class Qt < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "gperf" => :build
-  uses_from_macos "llvm" => :test # Our test relies on `clang++` in `PATH`.
 
   uses_from_macos "cups"
   uses_from_macos "krb5"
